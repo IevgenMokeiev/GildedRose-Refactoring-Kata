60f0c2e70e17d8b9baf726d9dd9f48aaa965c060
Eugene Mokeiev <ievgen.mokeiev@justeattakeaway.com>
2023-02-03 14:27:01 +0200

    complete happy path test coverage

---
+++ Sources/GildedRose/ItemType.swift
@@ -0,0 +1,12 @@
+//
+//  ItemType.swift
+//  
+//
+//  Created by Yevhen Mokeiev on 03.02.2023.
+//
+
+enum ItemType: String {
+    case agedBrie = "Aged Brie"
+    case backstagePass = "Backstage passes to a TAFKAL80ETC concert"
+    case sulfuras = "Sulfuras, Hand of Ragnaros"
+}
--- Tests/GildedRoseTests/GildedRoseTests.swift
+++ Tests/GildedRoseTests/GildedRoseTests.swift
@@ -7,6 +7,7 @@ class GildedRoseTests: XCTestCase {
         let items = [Item(name: "generic", sellIn: 2, quality: 2)]
         let app = GildedRose(items: items)
         app.updateQuality()
+
         let item = app.items[0]
         XCTAssertEqual(item.name, "generic")
         XCTAssertEqual(item.sellIn, 1)
@@ -17,6 +18,7 @@ class GildedRoseTests: XCTestCase {
         let items = [Item(name: "generic", sellIn: 0, quality: 2)]
         let app = GildedRose(items: items)
         app.updateQuality()
+
         let item = app.items[0]
         XCTAssertEqual(item.name, "generic")
         XCTAssertEqual(item.sellIn, -1)
@@ -28,6 +30,7 @@ class GildedRoseTests: XCTestCase {
         let app = GildedRose(items: items)
         app.updateQuality()
         app.updateQuality()
+
         let item = app.items[0]
         XCTAssertEqual(item.name, "generic")
         XCTAssertEqual(item.sellIn, -2)
@@ -38,9 +41,87 @@ class GildedRoseTests: XCTestCase {
         let items = [Item(name: "Aged Brie", sellIn: 2, quality: 2)]
         let app = GildedRose(items: items)
         app.updateQuality()
+
         let item = app.items[0]
         XCTAssertEqual(item.name, "Aged Brie")
         XCTAssertEqual(item.sellIn, 1)
         XCTAssertEqual(item.quality, 3)
+    }
+
+    func test_whenAgedBrie_thenCannotGoAboveQualityThreshold() {
+        let items = [Item(name: "Aged Brie", sellIn: 2, quality: 50)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let item = app.items[0]
+        XCTAssertEqual(item.name, "Aged Brie")
+        XCTAssertEqual(item.sellIn, 1)
+        XCTAssertEqual(item.quality, 50)
+    }
+
+    func test_whenSulfuras_thenDontDegradeDateAndQuiality() {
+        let items = [Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 3, quality: 40)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let item = app.items[0]
+        XCTAssertEqual(item.name, "Sulfuras, Hand of Ragnaros")
+        XCTAssertEqual(item.sellIn, 3)
+        XCTAssertEqual(item.quality, 40)
+    }
+
+    func test_whenBackstagePass_andMoreThanTenDays_thenQuialityIncreasesByOne() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 11, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let item = app.items[0]
+        XCTAssertEqual(item.name, "Backstage passes to a TAFKAL80ETC concert")
+        XCTAssertEqual(item.sellIn, 10)
+        XCTAssertEqual(item.quality, 11)
+    }
+
+    func test_whenBackstagePass_andTenDaysOrLess_thenQuialityIncreasesByTwo() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let item = app.items[0]
+        XCTAssertEqual(item.name, "Backstage passes to a TAFKAL80ETC concert")
+        XCTAssertEqual(item.sellIn, 9)
+        XCTAssertEqual(item.quality, 12)
+    }
+
+    func test_whenBackstagePass_andFiveDaysOrLess_thenQuialityIncreasesByThree() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let item = app.items[0]
+        XCTAssertEqual(item.name, "Backstage passes to a TAFKAL80ETC concert")
+        XCTAssertEqual(item.sellIn, 4)
+        XCTAssertEqual(item.quality, 13)
+    }
+
+    func test_whenBackstagePass_andAfterSellIn_thenQuialityDropsToZero() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 0, quality: 10)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let item = app.items[0]
+        XCTAssertEqual(item.name, "Backstage passes to a TAFKAL80ETC concert")
+        XCTAssertEqual(item.sellIn, -1)
+        XCTAssertEqual(item.quality, 0)
+    }
+
+    func test_whenBackstagePass_thenCannotGoAboveQualityThreshold() {
+        let items = [Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 2, quality: 50)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let item = app.items[0]
+        XCTAssertEqual(item.name, "Backstage passes to a TAFKAL80ETC concert")
+        XCTAssertEqual(item.sellIn, 1)
+        XCTAssertEqual(item.quality, 50)
     }
 }
